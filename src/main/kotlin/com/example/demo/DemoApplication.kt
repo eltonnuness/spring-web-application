47de3390d103e77c28eb47f76c11652288b3c465
Elton <eltonnuness@users.noreply.github.com>
2022-02-16 10:39:04 -0300

    feat: add comment

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -6,6 +6,7 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class DemoApplication
 
+//comment
 fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
 }
